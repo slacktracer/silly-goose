3543426db61f543409219bb84398fc2da2fed5ca
slacktracer <slacktracer@gmail.com>
2024-08-02 22:32:08 -0300

    Trying better GLSL support.

--- src/lib/modules/sg/three/grid-fragment-shader.glsl
+++ src/lib/modules/sg/three/grid-fragment-shader.glsl
@@ -3,17 +3,17 @@ uniform float uDivisions;
 varying vec2 vUv;
 
 float gridLine(vec2 pos, float width) {
-    vec2 grid = abs(fract(pos - 0.5) - 0.5) / fwidth(pos);
-    return 1.0 - min(min(grid.x, grid.y), 1.0);
+  vec2 grid = abs(fract(pos - 0.5) - 0.5) / fwidth(pos);
+  return 1.0 - min(min(grid.x, grid.y), 1.0);
 }
 
 void main() {
-    vec2 pos = vUv * uGridSize * uDivisions;
+  vec2 pos = vUv * uGridSize * uDivisions;
 
-    float majorGrid = gridLine(pos / uDivisions, 1.0) * 0.5;
-    float minorGrid = gridLine(pos, 1.0) * 0.15;
+  float majorGrid = gridLine(pos / uDivisions, 1.0) * 0.5;
+  float minorGrid = gridLine(pos, 1.0) * 0.15;
 
-    float grid = max(majorGrid, minorGrid);
+  float grid = max(majorGrid, minorGrid);
 
-    gl_FragColor = vec4(vec3(1.0), grid);
+  gl_FragColor = vec4(vec3(1.0), grid);
 }
